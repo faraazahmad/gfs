f7dbcbbfa338c7e5dceb0bae256ef841c3284c3b
Syed Faraaz Ahmad <faraaz98@live.com>
2024-12-16 21:12:32 +0530

    feat: manager rest api

--- lib/manager/rest_api.ex
+++ lib/manager/rest_api.ex
@@ -1,14 +1,16 @@
 defmodule Gfs.Manager.RestApi do
   use Plug.Router
 
-  plug Plug.Logger
-  plug Plug.Parsers,
+  plug(Plug.Logger)
+
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Jason
+  )
 
-  plug :match
-  plug :dispatch
+  plug(:match)
+  plug(:dispatch)
 
   get "/" do
     send_resp(conn, 200, "OK")
@@ -16,11 +18,18 @@ defmodule Gfs.Manager.RestApi do
 
   get "/chunk_servers" do
     chunk_servers = Gfs.Manager.Repo.all(Gfs.Schema.Node)
-    # send_resp(conn, 200, chunk_servers)
 
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, Jason.encode!(chunk_servers))
+  end
+
+  get "/files" do
+    send_resp(conn, 300, "Not Implemented")
+  end
+
+  get "/file/:file_name" do
+    send_resp(conn, 300, "Not Implemented")
   end
 
   match _ do
