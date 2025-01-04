1c4010623d4476c4f185ed163c7c3e4e47819c55
Faraaz Ahmad <faraaz98@live.com>
2025-01-04 21:33:18 +0000

    feat: add read and append routes to chunk server

--- lib/chunk_server/rest_api.ex
+++ lib/chunk_server/rest_api.ex
@@ -17,7 +17,25 @@ defmodule Gfs.ChunkServer.RestApi do
   end
 
   get "/chunk/:chunk_id" do
-    send_resp(conn, 300, "Not Implemented")
+    chunk_file_path = Path.expand("~/.gfs/chunk_server/chunks/#{chunk_id}")
+    file_content = case File.read(chunk_file_path) do
+      {:ok, content} -> content
+      {:error, error} -> error
+    end
+
+    send_resp(conn, 200, Jason.encode!(file_content))
+  end
+
+  put "append/chunk/:chunk_id" do
+    content = conn.body.content
+    chunk_file_path = Path.expand("~/.gfs/chunk_server/chunks/#{chunk_id}")
+    file_content = case File.read(chunk_file_path) do
+      {:ok, binary_content} -> binary_content
+      {:error, _} -> nil
+    end
+
+    reply = File.write(chunk_file_path, "#{file_content}#{content}")
+    send_resp(conn, 200, Jason.encode!(reply))
   end
 
   match _ do
