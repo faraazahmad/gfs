18c6350a63cb30862b856b8cae215dfc60b68d22
Faraaz Ahmad <faraaz98@live.com>
2025-04-05 18:04:52 +0000

    fix: get free port for chunkserver

--- lib/chunk_server/app.ex
+++ lib/chunk_server/app.ex
@@ -14,10 +14,18 @@ defmodule Gfs.ChunkServer.App do
 
     children = [
       Gfs.ChunkServer.Repo,
-      {Bandit, plug: Gfs.ChunkServer.RestApi},
+      {Bandit, plug: Gfs.ChunkServer.RestApi, scheme: :http, port: get_free_port()},
       # Gfs.Task.MonitorNodes
     ]
     IO.puts "Starting GFS ChunkServer Application"
     Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+  def get_free_port do
+    {:ok, socket} = :gen_tcp.listen(0, [:binary, {:active, false}, {:reuseaddr, true}])
+    {:ok, port} = :inet.port(socket)
+    :gen_tcp.close(socket)
+
+    port
   end
 end
